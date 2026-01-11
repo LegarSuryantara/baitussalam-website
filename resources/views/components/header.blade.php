307508b9b1b60a1d52297aa73d16c11abf90a2a0
LegarSuryantara <legarsuryantara@gmail.com>
2026-01-11 22:44:28 +0700

    frontend profile

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,27 @@
+<div class="containerHeader">
+    <div class="searchBarHearder bg-success-subtle">
+        <div class="headerchild container-fluid d-flex flex-row align-items-center ">
+            <div class="navbarTop">
+                <x-Navbar></x-Navbar>
+            </div>
+            <div class="Logo m-3">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/images/logo.png" alt="BaitussalamLogo" width="120" height="96">
+                </a>
+            </div>
+            <div class="input-group mx-auto w-50" id="searchBarDesktop">
+                <x-SearchBar></x-SearchBar>
+            </div>
+            <div class="fotoProfileContainer">
+                <a href="">
+                    <img class="rounded-circle m-3" src="assets/images/fotoProfile.jpg" alt="fotoProfile" width="50"
+                        height="50">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="navbar1">
+        <x-Navbar></x-Navbar>
+    </div>
+
+</div>
