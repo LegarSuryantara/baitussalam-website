f4e457313a6c94ad0a0233405e6dd9d95922aaec
dikydharmawan <dikyaye118@gmail.com>
2026-03-18 15:56:31 +0700

    style: apply modern aesthetic to fullcalendar, fix mobile typography

--- resources/views/dokumen_masjid/dokumenPage.blade.php
+++ resources/views/dokumen_masjid/dokumenPage.blade.php
@@ -49,11 +49,8 @@
                 </div>
             </div>
 
-<<<<<<< HEAD:resources/views/dokumen_masjid/dokumenPage.blade.php
             <hr>
 
-=======
->>>>>>> origin/main:resources/views/dokumenPage.blade.php
         </div>
     </div>
 </x-layout>
